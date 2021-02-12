04f8d26ab636df79cc155a922e480d0aba72e909
mario <orlando_sam@hotmail.com>
2021-02-11 17:47:11 -0700

    Improve code for VowelCount

--- TGS.Challenge.Tests/VowelCountTests.cs
+++ TGS.Challenge.Tests/VowelCountTests.cs
@@ -18,6 +18,13 @@ namespace TGS.Challenge.Tests
             Assert.Throws<ArgumentException>(() => _vowelCount.Count(string.Empty));
         }
 
+
+        [Fact]
+        public void ValueIsNull_IsRequired()
+        {
+            Assert.Throws<ArgumentException>(() => _vowelCount.Count(null));
+        }
+
         [Fact]
         public void AEIOU_Returns_Correct_Count()
         {
@@ -51,5 +58,30 @@ namespace TGS.Challenge.Tests
 
             Assert.Equal(count, 6);
         }
+
+        [Fact]
+        public void AaEeIiOoUu_Returns_Correct_Count()
+        {
+            var count = _vowelCount.Count("AaEeIiOoUu");
+
+            Assert.Equal(count, 10);
+        }
+
+        [Fact]
+        public void symbols_Returns_Correct_Count()
+        {
+            var count = _vowelCount.Count("&/(&(&%·(()//&%$··$$·%$");
+
+            Assert.Equal(count, 0);
+        }
+
+        [Fact]
+        public void bigstring_Returns_Correct_Count()
+        {
+            var count = _vowelCount.Count("Childhood Cancer Society (CCS) began with the vision of a 16-year-old high school student who realized that having family around him while he was being evaluated for childhood leukemia created the support that he needed to cope. There is nothing more powerful than the miracles that result from the love of family, and it is the mission of CCS to see to it that children diagnosed with cancer have the opportunity to experience the benefits that come from this vital support system. A true miracle happens when a child looks up from a hospital bed and feels the comfort of loved ones at");
+
+            Assert.Equal(count, 180);
+        }
+       
     }
 }
